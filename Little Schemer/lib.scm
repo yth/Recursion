de33e1954d36ee4c31a49a309288f4f6a1bdbc04
Yuan Cao <cao.yuan1@northeastern.edu>
2021-06-01 17:58:24 -0400

    Add new functions to lib and tests to individual chapters

--- Little Schemer/lib.scm
+++ Little Schemer/lib.scm
@@ -1,3 +1,11 @@
 (define atom?
 	(lambda (x)
 		(and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  	(lambda (x)
+	  	(cond
+		  	((null? x) #t)
+			((atom? (car x)) (lat? (cdr l)))
+			(else #f))))
+
