6c72bfa2852cf91eede229542d5488f64c503f0c
Yuan Cao <yuan.cao.email@gmail.com>
2021-06-02 17:57:43 -0400

    Add member?

--- Little Schemer/lib.scm
+++ Little Schemer/lib.scm
@@ -3,9 +3,15 @@
 		(and (not (pair? x)) (not (null? x)))))
 
 (define lat?
-  	(lambda (x)
-	  	(cond
-		  	((null? x) #t)
+	(lambda (x)
+		(cond
+			((null? x) #t)
 			((atom? (car x)) (lat? (cdr l)))
 			(else #f))))
 
+(define member?
+	(lambda (a lat)
+		((null? lat) #f)
+		(else (or
+			(eq? (car lat) a)
+			(member? a (cdr lat))))))
